08c29d510ff873575cc18d3fb174b82ae78d3f3a
ValeriaMartchenko <95330714+ValeriaMartchenko@users.noreply.github.com>
2021-12-05 21:35:43 -0800

    Added dynamic Navigation Bar

--- InnovationKeyboards/scripts.js
+++ InnovationKeyboards/scripts.js
@@ -1,26 +1,52 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var mainMenu = {
+    "items": [
+        {
+            "id": "home",
+            "link": "index.html",
+            "text": "Home"
+        },
+        {
+            "id": "laptops",
+            "link": "PaperLaptops.html",
+            "text": "Paper Laptops"
+        },
+        {
+            "id": "blog",
+            "link": "Blog.html",
+            "text": "Blog"
+        },
+        {
+            "id": "contact",
+            "link": "mailto:s-olaksberg@lwsd.org",
+            "text": "Contact Us",
+            "dropdown": {
+                "link": "mailto:s-olaksberg@lwsd.org",
+                "text": "s-olaksberg@lwsd.org"
+            }
+        },
+        {
+            "id": "order",
+            "link": "Form.html",
+            "text": "Get Your Keyboard"
+        },
+    ]
+};
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+function renderMenu(menuId, active) {
+    var menu = document.getElementById(menuId);
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+    var html = '<ul class="NavigationBar">';
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    for (let i = 0; i < mainMenu.items.length; i++) {
+        let item = mainMenu.items[i];
+        html += `<li class="${active == item.id ? 'active' : 'NavLi'}"><div class="dropdown"><a class="NavA" href="${item.link}">${item.text}</a>`;
+        if (!!item.dropdown) {
+            html += `<div class="dropdown-content"><a href="${item.dropdown.link}" target="_blank">${item.dropdown.text}</a></div>`
+        }
+        html += '</div></li>';
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+
+    html += '</ul>';
+
+    menu.innerHTML = html;
 }
